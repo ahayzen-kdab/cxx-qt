c0ba6fe36b509b2b7f162392414167e1c9ed0c8e
Andrew Hayzen <andrew.hayzen@kdab.com>
2025-09-08 11:10:15 +0100

    threading: fix `mutable reference to C++ type requires a pin` in drop

--- crates/cxx-qt-gen/src/generator/rust/threading.rs
+++ crates/cxx-qt-gen/src/generator/rust/threading.rs
@@ -89,7 +89,7 @@ pub fn generate(
                     #[doc(hidden)]
                     #(#thread_drop_attrs)*
                     #(#cfgs)*
-                    fn #thread_drop_name(cxx_qt_thread: &mut #cxx_qt_thread_ident);
+                    fn #thread_drop_name(cxx_qt_thread: Pin<&mut #cxx_qt_thread_ident>);
 
                     #[doc(hidden)]
                     #(#thread_is_destroyed_attrs)*
@@ -154,7 +154,7 @@ pub fn generate(
                     }
 
                     #[doc(hidden)]
-                    fn threading_drop(cxx_qt_thread: &mut #module_ident::#cxx_qt_thread_ident)
+                    fn threading_drop(cxx_qt_thread: Pin<&mut #module_ident::#cxx_qt_thread_ident>)
                     {
                         #thread_drop_qualified(cxx_qt_thread);
                     }
